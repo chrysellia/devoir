0c0f40d9d6546092c46ddb41dc1da50978b0bf98
Chrysellia <priscilliachryso@gmail.com>
2020-09-06 17:25:34 +0200

    Mapping Content.js

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -4,21 +4,229 @@ import './Content.styles.scss';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
+import ProductList from '../Product/ProductList';
+import { Link } from 'react-router-dom';
 
 class Content extends Component {
-  // state = {
-  //   selectedProduct: {},
-  // }
+  state = {
+     produits : [{
+      titre:'CHEMISE',
+      image: './image/shirt-female.jpg',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt4.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt2.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt3.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+     },
 
-  // showProduct = () => {
-  //   this.setState({
-  //     selectedProduct: product
-  //   });
-  // }
+      {titre:'ROBE',
+      image: './image/dress-female.jpg',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt1.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+      },
+      
+      {
+      titre:'JUPE',
+      image: './image/skirt-female.jpg',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt1.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+      },
+    
+      {
+      titre:'SHORT',
+      image: './image/short-female.jpg',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt1.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+      },
+      
+      {titre:'BLOUSON',
+      image: './image/blouson-female.jpg',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt1.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+      },
+      
+      {titre:'PANTS',
+      image: './image/pants-female.webp',
+      products: [{
+        nom: 'Chemisier en mousseline de soie florale',
+        description: 'Chemisier femmes 2019 dames hauts en mousseline de soie florale Blouse pour haut pour femme hauts volants support papillon manches blusas creux',
+        prix: 9.29,
+        category: 'Chemisier femme',
+        image: './image/shirt1.jpg',
+        }, {
+            nom: 'Long sleeve blue-white',
+            description: 'Long sleeve blue-white loose Korean style V-neck shirt',
+            prix: 8.75,
+            category: 'Chemisier femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Summer butterfly Sleeve',
+            description: 'Summer casual solid women Chiffon Shirt Female butterfly Sleeve Korean loose size women blouse top black s',
+            prix: 67.043,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        },
+        {
+            nom: 'Mode Hiver Coréen',
+            description: 'Casual Work style (vest over white shirt)',
+            prix: 12.00,
+            category: 'Chemise femme',
+            image: './image/shirt1.jpg',
+        } 
+      ],
+      }
+     ],
+
+     selectedProduct: {},
+     showDetail: false
+  }
+
+  showProduit = (produit) => {
+    this.setState({
+      selectedProduct: produit,
+      showDetail: true
+    })
+  }
+
+  hideProduit = () => {
+        this.setState({
+            showDetail: false
+        }, () => {
+            document.body.style.overflow = 'auto';
+        })
+    }
+
 
     render() {
-      // const {selectedProduct} = this.state;
-      // const { produits } =this.props;
+      const { produits, selectedProduct, showDetail } = this.state;
         return(
           <>
             <section>
@@ -43,48 +251,39 @@ class Content extends Component {
                 <MiddleBody />
               </Slide>
               <h1 className="titre sm:text-3xl text-2xl text-center font-medium mb-4 text-center text-pink-800">NOS PRODUITS</h1>
-              <div class="container px-5 py-24 mx-auto flex flex-wrap">
-                <div class="flex flex-wrap md:-m-2 -m-1">
-                  <div class="flex flex-wrap w-1/2">
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/dress-female.jpg"/>
-                      <h1 className="category_product text-center">ROBE</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/short-female.jpg"/>
-                      <h1 className="category_product text-center">SHORT</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-full relative"onClick={this.showProduct}>
-                      <img alt="gallery" class="product_image w-full h-full object-cover object-center block" src="image/shirt-female.jpg"/>
-                      <h1 className="category_product text-center">CHEMISE</h1>
-                    </div>
+              <section className="product_container relative">
+                  <div className="container py-8 md:py-10 lg:py-24 mx-auto">  
+                      <div className="flex flex-wrap justify-center">
+                          { produits.map((produit, key) => {
+                              return (
+                                  <div key={key} className="product-box w-full xl:w-1/3 md:w-1/3 px-4 md:px-1 py-2">
+                                      <div onClick={() => this.showProduit(produit)} className="product-border bg-gray-100 relative" style={{height: '375px'}}>
+                                          <img
+                                              className="h-full w-full absolute w-full object-cover object-center"
+                                              src={produit.image} alt="Cocktails de fruit" />
+                                          <div className="w-full absolute flex bg-black items-center justify-center bottom-0 bg-opacity-25">
+                                              {/* <NavLink to={produit.url}> */}
+                                                  <h2 className="text-center product-title">
+                                                      { String(produit.titre).charAt(0).toUpperCase() + String(produit.titre).slice(1).toLowerCase() }
+                                                  </h2>
+                                              {/* </NavLink> */}
+                                          </div>
+                                      </div>
+                                  </div>
+                              )
+                          }) }
+                      </div>
                   </div>
-                  <div class="flex flex-wrap w-1/2 relative">
-                    <div class="md:p-2 p-1 w-full">
-                      <img alt="gallery" class="product_image w-full h-full object-cover object-center block" src="image/skirt-female.jpg"/>
-                      <h1 className="category_product text-center">JUPE</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/pants-female.webp"/>
-                      <h1 className="category_product text-center">PANTS</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/blouson-female.jpg"/>
-                      <h1 className="category_product text-center">BLOUSON</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/sac-female.jpg"/>
-                      <h1 className="category_product text-center">SAC</h1>
-                    </div>
-                    <div class="md:p-2 p-1 w-1/2 relative">
-                      <img alt="gallery" class="product_image w-full object-cover h-full object-center block" src="image/crop.jpg"/>
-                      <h1 className="category_product text-center">CROP</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                </section>
             </div>
           </section>
+          <div className="absolute">
+              { showDetail ? (<ProductList 
+                  selectedProduct={selectedProduct}
+                  hideProduit={this.hideProduit}/>
+                  ) :
+              null}
+          </div>
           </>
         )
     }
--- src/component/Product/ProductList.js
+++ src/component/Product/ProductList.js
@@ -3,57 +3,32 @@ import React, { Component } from 'react';
 
 class ProductList extends Component {
     render() {
-        
+        const { selectedProduct } = this.props;
+        const { products } = selectedProduct;
         return(
             <>
             <div>
                 <h1 className="text-center titre pt-20 pb-0">Liste des produits</h1>
                 <section>
-                    <div class="container px-5 py-24 mx-auto">
-                        <div class="flex flex-wrap -m-4">
-                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a class="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="image/shirt4.jpg"/>
-                            </a>
-                            <div class="mt-4">
-                                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Chemisier femme</h3>
-                                <h2 class="text-gray-900 title-font text-lg font-medium">Chemisier en mousseline de soie florale</h2>
-                                <p class="mt-1">€ 9,29</p>
+                    { products.map( product => {
+                        return(
+                            <div class="container px-5 py-24 mx-auto">
+                                <div class="flex flex-wrap -m-4">
+                                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                        <a class="block relative h-48 rounded overflow-hidden">
+                                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="image/shirt4.jpg"/>
+                                        </a>
+                                        <div class="mt-4">
+                                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{ product.categroy}</h3>
+                                            <h2 class="text-gray-900 title-font text-lg font-medium">{ product.nom}</h2>
+                                            <p class="mt-1">€ { product.prix}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="image/shirt2.jpg"/>
-                            </a>
-                            <div class="mt-4">
-                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Chemisier femme</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Long sleeve blue-white</h2>
-                            <p class="mt-1">$8.75</p>
-                            </div>
-                        </div>
-                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="image/shirt3.jpg"/>
-                            </a>
-                            <div class="mt-4">
-                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Chemisier femme</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Summer butterfly Sleeve</h2>
-                            <p class="mt-1">$67,043</p>
-                            </div>
-                        </div>
-                        <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="image/shirt1.jpg"/>
-                            </a>
-                            <div class="mt-4">
-                            <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">Chemisier femme</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Mode Hiver Coréen</h2>
-                            <p class="mt-1">$12.00</p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </section>
+                        )
+                    })}
+                </section>
             </div>
             </>
         )
